041dad325240162b5908727a218b792049aaf3f2
Ranjeet Sengar <sengar6725@gmail.com>
2022-08-22 23:18:39 +0530

    new projects wip ——————————————— new projects wip

---
+++ build/minance-web-project.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php include "header.php" ?>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php include "work-head.php" ?>
+    <section id="p-menu">
+      <span class="p-overlay"></span>
+      <div class="p-menu-container">
+        <span class="p-line top"></span>
+        <span class="p-line middle left"></span>
+        <span class="p-line middle right"></span>
+        <span class="p-line bottom"></span>
+      </div>
+    </section>
+
+   
+    <?php include "footer.php" ?>
+     <script src="js/work.js"></script>
+
+
+    
+  </body>
+</html>
